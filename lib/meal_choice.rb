80526378f683a38d8aaf709551ce37736b802c46
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-05 04:09:07 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,6 +2,6 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(veg1, veg2, protein = "meat")
-  return "What a nutritious meal! /n A plate of #{protein} with #{veg1} and #{veg2}."
+  return "What a nutritious meal!\nA plate of #{protein} with #{veg1} and #{veg2}."
   
 end
